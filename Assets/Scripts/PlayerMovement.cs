fc1bc5dcbca3c63cc67add8dc63b5a338595571b
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-02-19 11:01:35 +0700

    Add player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp("space"))
+        float dirX = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(dirX * 5f, rigidbody2D.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,14,0);
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 6f);
         }
     }
 }
