07dae817be2a344fc917e14baeaa952d1e8ade0a
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-02-19 21:48:17 +0700

    Added animation, Camera Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,53 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private float dirX = 0;
+    [SerializeField] private float moveState = 5f;
+    [SerializeField] private float jumpForce = 6f;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
-        rigidbody2D.velocity = new Vector2(dirX * 5f, rigidbody2D.velocity.y);
+        dirX = movementState();
+        animationState();
+    }
+    private float movementState() 
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rigidbody2D.velocity = new Vector2(dirX * moveState, rigidbody2D.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 6f);
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+        }
+
+        return dirX;
+    }
+    private void animationState()
+    {
+        if (dirX > 0f)
+        {
+            animator.SetBool("is_running", true);
+            spriteRenderer.flipX = false;
+        }
+        else if(dirX < 0f)
+        {
+            animator.SetBool("is_running", true);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.SetBool("is_running", false);
         }
     }
 }
