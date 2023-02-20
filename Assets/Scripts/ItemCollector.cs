9e840493f66c2d3b20ea0430494ba361226b4eeb
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-02-20 20:29:55 +0700

    Collect and Scoring

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int fruits = 0;
+    [SerializeField] private Text scoreText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fruit"))
+        {
+            Destroy(collision.gameObject);
+            fruits++;
+            scoreText.text = "Scores : "+fruits;
+        }      
+    }
+}
